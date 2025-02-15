48f5a175389f22d3a86909b490d84955a272742c
Taufik Afandi <mohtaufikafandi@gmail.com>
2025-02-15 21:49:00 +0700

    update

--- index.ts
+++ index.ts
@@ -11,34 +11,41 @@ type Wisata_Data = {
   province: string;
   images: {
     url: string;
+    author: {
+      name: string;
+      link: string;
+      photoUrl: string;
+    };
   }[];
 };
 
 // Main function
 const main = async () => {
   // File path
-  const FILE_PATH = `data/wisata.json`;
+  const FILE_PATH = `data/wisata-v2.json`;
+  const LOG_PATH = `data/daerah-log.json`;
 
   // Fetch kabupaten kota data
   const kabupatenKotaData = await fetchDataKabupatenKota();
+  console.log('Fetching data for', kabupatenKotaData.length, 'daerah...');
+
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
 
   for (const kabupatenKota of kabupatenKotaData) {
     // Data to be populated
     const WISATA_DATA: Wisata_Data[] = [];
 
     console.log(
-      `[${new Date().toLocaleTimeString()}] Scraping data for: `,
+      `[${new Date().toLocaleTimeString()}] Scraping data for:`,
       kabupatenKota
     );
 
     // Search key
     const SEARCH_KEY = `Wisata ${kabupatenKota}`;
-
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({
-      headless: true,
-    });
-    const page = await browser.newPage();
 
     // Navigate the page to a URL
     await page.goto('https://www.google.com/maps');
@@ -65,18 +72,6 @@ const main = async () => {
 
       let index = 0;
       while (true) {
-        // Progress bar for terminal
-        const progressPercentage = Math.floor(
-          (index / allResults.length) * 100
-        );
-        process.stdout.clearLine(0);
-        process.stdout.cursorTo(0);
-        process.stdout.write(
-          `Progress: ${drawProgressBar(progressPercentage)} [${index}/${
-            allResults.length
-          }]`
-        );
-
         const item = allResults[index];
 
         if (!item) {
@@ -149,126 +144,267 @@ const main = async () => {
             .then((val) => val ?? '')
         ).replace('JSHandle:', '');
 
-        // Click the anchor element
-        await anchorElem.click();
-        await customDelay(200);
-        await anchorElem.click();
-
-        // Wait for the details to load
-        const panel = await page
-          .waitForSelector(`div[role="main"][aria-label="${title}"]`, {
-            visible: true,
-            timeout: 10000,
-          })
-          .catch(() => {
-            return null;
-          });
-
-        if (!panel) {
-          index++;
-          continue;
-        }
-
-        // DATA: about
-        const about = await page
-          .waitForSelector(
-            `div[role="region"][aria-label="Tentang ${title}"] div.fontBodyMedium div>div:first-child`,
-            { visible: true, timeout: 2000 }
-          )
-          .then(async (elem) => {
-            return elem ? await elem.getProperty('innerText') : null;
-          })
-          .then((prop) => (prop ? prop.jsonValue() : ''))
-          .catch(() => '');
-
-        // DATA: address
-        const address = await page
-          .waitForSelector(
-            `div[role="region"] button[data-tooltip="Salin alamat"] div.fontBodyMedium`,
-            { visible: true, timeout: 2000 }
-          )
-          .then(async (elem) => {
-            return elem ? await elem.getProperty('innerText') : null;
-          })
-          .then((prop) => (prop ? prop.jsonValue() : ''))
-          .catch(() => '');
-
-        // DATA: images
-        const images = [];
-        const previewImage = await item.$('img');
-        if (previewImage) {
-          const previewImageUrl = await previewImage
-            .getProperty('src')
-            .then((prop) => prop.jsonValue())
-            .then((val) => val ?? null);
-
-          if (previewImageUrl.includes('streetview')) {
-            index++;
-            continue;
-          }
-
-          const imageCarousel = await page
-            .waitForSelector(
-              `div[aria-roledescription="carousel"][aria-label="Foto ${title}"]`,
-              { visible: true, timeout: 2000 }
-            )
-            .then(async (elem) => {
-              return elem ? await elem.$$(`button>img`) : [];
-            })
-            .catch(() => []);
-
-          for (const image of imageCarousel) {
-            const imageUrl = await image
-              .getProperty('src')
-              .then((prop) => prop.jsonValue())
-              .then((val) => val ?? null);
-
-            // Check if the 4 last characters are k-no
-            if (imageUrl.split('').slice(-4).join('') === 'k-no') {
-              const formattedImageUrl = imageUrl
-                .split('=')
-                .map((val, i) => {
-                  if (i === 1) {
-                    return 's1920-k-no';
-                  }
-
-                  return val;
-                })
-                .join('=');
-
-              if (imageUrl) {
-                images.push({
-                  url: formattedImageUrl,
-                });
-              }
-            }
-          }
-        }
-
         WISATA_DATA.push({
           title,
-          about,
-          address,
+          about: '',
+          address: '',
           rating,
           reviewers,
-          images,
+          images: [],
           link,
           province: kabupatenKota,
         });
 
-        await customDelay(1000);
         index++;
       }
     }
 
-    setTimeout(async () => {
-      // Close the browser
-      await browser.close();
-    }, 3000);
+    console.log(`Populating images for ${WISATA_DATA.length} data...`);
+
+    // DATA: Images, About, Address
+    const WISATA_DATA_WITH_IMAGES: Wisata_Data[] = [];
+    let wisataIndex = 0;
+    while (wisataIndex < WISATA_DATA.length) {
+      const wisataTarget = WISATA_DATA[wisataIndex];
+
+      // Progress bar
+      const progressPercentage = Math.floor(
+        ((wisataIndex + 1) / WISATA_DATA.length) * 100
+      );
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      process.stdout.write(`Progress: ${drawProgressBar(progressPercentage)}`);
+
+      // Navigate to the link
+      const pageStatus = await page
+        .goto(wisataTarget.link, { timeout: 15000 })
+        .catch(() => null);
+      if (!pageStatus) {
+        wisataIndex++;
+        continue;
+      }
+
+      // Waiting for Gallery element
+      const galleryElem = await page
+        .waitForSelector(
+          `div[aria-label="${wisataTarget.title}"][role="main"] button[aria-label="Foto ${wisataTarget.title}"]`,
+          { timeout: 10000 }
+        )
+        .catch(() => null);
+
+      // DATA: about
+      const about = await page
+        .waitForSelector(
+          `div[role="region"][aria-label="Tentang ${wisataTarget.title}"] div.fontBodyMedium div>div:first-child`,
+          { visible: true, timeout: 2000 }
+        )
+        .then(async (elem) => {
+          return elem ? await elem.getProperty('innerText') : null;
+        })
+        .then((prop) => (prop ? prop.jsonValue() : ''))
+        .catch(() => '');
+
+      // DATA: address
+      const address = await page
+        .waitForSelector(
+          `div[role="region"] button[data-tooltip="Salin alamat"] div.fontBodyMedium`,
+          { visible: true, timeout: 2000 }
+        )
+        .then(async (elem) => {
+          return elem ? await elem.getProperty('innerText') : null;
+        })
+        .then((prop) => (prop ? prop.jsonValue() : ''))
+        .catch(() => '');
+
+      if (!galleryElem) {
+        wisataIndex++;
+        continue;
+      }
+
+      await page.evaluate((galleryElem) => galleryElem.click(), galleryElem);
+
+      // Waiting for Gallery panel
+      const galleryPanelElem = await page
+        .waitForSelector(
+          `div[aria-label="Foto ${wisataTarget.title}"][role="main"]`,
+          { timeout: 10000 }
+        )
+        .catch(() => null);
+      // Waiting for the first gallery item
+      const firstGalleryItem = await page
+        .waitForSelector(
+          `div[aria-label="Foto ${wisataTarget.title}"][role="main"] div:last-child>div:first-child div>div>a[href="#"]`,
+          { timeout: 10000 }
+        )
+        .catch(() => null);
+
+      if (!galleryPanelElem || !firstGalleryItem) {
+        wisataIndex++;
+        continue;
+      }
+
+      // Get all gallery items
+      const galleryItems = await galleryPanelElem.$$(
+        'div:last-child>div:first-child div>div>a[href="#"]'
+      );
+
+      const filteredGalleryItems = await Promise.all(
+        galleryItems.filter(async (item) => {
+          const isVideo = await item.$(`div[role="img"]>div.fontLabelMedium`);
+
+          if (isVideo) {
+            return false;
+          }
+
+          return true;
+        })
+      );
+
+      try {
+        // Iterate through all 5 gallery items
+        const galleryResults: Wisata_Data['images'][0][] = [];
+        let galleryItemIndex = 0;
+        while (
+          galleryResults.length < 5 &&
+          galleryItemIndex < filteredGalleryItems.length
+        ) {
+          let resultItem: Wisata_Data['images'][0] = {
+            url: '',
+            author: {
+              name: '',
+              link: '',
+              photoUrl: '',
+            },
+          };
+
+          const galleryItem = filteredGalleryItems[galleryItemIndex];
+          await page.evaluate(
+            (galleryItem) => galleryItem.click(),
+            galleryItem
+          );
+          await galleryItem.click();
+
+          await customDelay(1000);
+
+          const imageBox = await galleryItem
+            .waitForSelector(`div[role="img"]>div.loaded`, { timeout: 1000 })
+            .catch(() => {
+              return null;
+            });
+
+          if (!imageBox) {
+            galleryItemIndex++;
+            continue;
+          }
+
+          const imageUrl = await imageBox
+            .evaluate((el) => {
+              return window.getComputedStyle(el).backgroundImage;
+            })
+            .then((res) => cleanBackgroundImageUrl(res));
+
+          if (imageUrl.split('').slice(-4).join('') !== 'k-no') {
+            galleryItemIndex++;
+            continue;
+          }
+
+          // Log image URL
+          resultItem.url = imageUrl
+            .split('=')
+            .map((val, i) => {
+              if (i === 1) {
+                return 's1080-k-no';
+              }
+
+              return val;
+            })
+            .join('=');
+
+          // Waiting for image owner element
+          const imageOwnerElem = await page
+            .waitForSelector(`div[role="navigation"] h2.fontBodySmall>span`, {
+              timeout: 3000,
+            })
+            .catch(() => null);
+
+          if (!imageOwnerElem) {
+            galleryItemIndex++;
+            continue;
+          }
+
+          // Determine uploader type
+          const isPlaceOwnerOrJustUser = await Promise.race([
+            page
+              .waitForSelector(
+                `div#titlecard div[role="navigation"] h1>span:last-child:has(a)`,
+                { timeout: 3000 }
+              )
+              .catch(() => null),
+            page
+              .waitForSelector(
+                `div[role="navigation"] h2.fontBodySmall>span:has(a)`,
+                { timeout: 3000 }
+              )
+              .catch(() => null),
+          ]);
+
+          if (isPlaceOwnerOrJustUser) {
+            const imageOwnerPhoto = await isPlaceOwnerOrJustUser
+              .waitForSelector('a:first-child>div', { timeout: 3000 })
+              .then((el) => {
+                return el
+                  ?.evaluate((el) => {
+                    return window.getComputedStyle(el).backgroundImage;
+                  })
+                  .then((res) => cleanBackgroundImageUrl(res));
+              })
+              .catch(() => null);
+
+            const imageOwnerProfileLink = await isPlaceOwnerOrJustUser
+              .waitForSelector('a:first-child', { timeout: 3000 })
+              .then((el) => {
+                return el?.evaluate((el) => {
+                  return el.getAttribute('href');
+                });
+              })
+              .catch(() => null);
+
+            const imageOwnerName = await isPlaceOwnerOrJustUser
+              .waitForSelector('a:last-child>span', { timeout: 3000 })
+              .then((el) => {
+                return el?.evaluate((el) => el.textContent);
+              })
+              .catch(() => null);
+
+            resultItem.author = {
+              photoUrl: imageOwnerPhoto ?? '',
+              link: imageOwnerProfileLink ?? '',
+              name: imageOwnerName ?? '',
+            };
+          }
+
+          if (resultItem.url !== '' && resultItem.author.name !== '') {
+            galleryResults.push(resultItem);
+          }
+
+          galleryItemIndex++;
+        }
+
+        WISATA_DATA_WITH_IMAGES.push({
+          ...wisataTarget,
+          about,
+          address,
+          images: galleryResults,
+        });
+
+        wisataIndex++;
+      } catch (error) {
+        wisataIndex++;
+      }
+    }
 
     // Save the data in json file
     let existingData: Wisata_Data[] = [];
-
     if (fs.existsSync(FILE_PATH)) {
       const rawData = fs.readFileSync(FILE_PATH, 'utf-8');
       existingData = JSON.parse(rawData);
@@ -277,14 +413,29 @@ const main = async () => {
     // Make sure there's no duplicate date by comparing title and address
     const updatedData = [
       ...new Map(
-        [...existingData, ...WISATA_DATA].map((item) => [item.link, item])
+        [...existingData, ...WISATA_DATA_WITH_IMAGES].map((item) => [
+          item.link,
+          item,
+        ])
       ).values(),
     ];
 
     fs.writeFileSync(FILE_PATH, JSON.stringify(updatedData));
 
+    // Log the daerah
+    const rawLogData = fs.readFileSync(LOG_PATH, 'utf-8');
+    const rawParsedLogData = JSON.parse(rawLogData);
+
+    fs.writeFileSync(
+      LOG_PATH,
+      JSON.stringify([kabupatenKota, ...rawParsedLogData], null, 2)
+    );
+
     console.log('\n');
   }
+
+  // Close the browser
+  await browser.close();
 };
 
 /**
@@ -305,6 +456,8 @@ const drawProgressBar = (progress: number) => {
  * @returns
  */
 const fetchDataKabupatenKota = async () => {
+  const LOG_PATH = `data/daerah-log.json`;
+
   const csv = await fetch(
     'https://raw.githubusercontent.com/kodewilayah/permendagri-72-2019/main/dist/base.csv',
     {
@@ -334,13 +487,15 @@ const fetchDataKabupatenKota = async () => {
       return `${val.name}, PROVINSI ${provinceName}`;
     });
 
-  // Shuffle the data
-  for (let i = formattedData.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [formattedData[i], formattedData[j]] = [formattedData[j], formattedData[i]];
-  }
+  const rawData = fs.readFileSync(LOG_PATH, 'utf-8');
+  const rawParsedData = JSON.parse(rawData);
 
-  return formattedData;
+  const filteredData = formattedData.filter(
+    (val) => !rawParsedData.includes(val)
+  );
+  // .filter((val) => val.toLowerCase().includes('jawa barat'));
+
+  return shuffleArray(filteredData);
 };
 
 /**
@@ -367,7 +522,7 @@ const scrollToEnd = async (page: Page, box: ElementHandle<HTMLDivElement>) => {
       }
     }, box);
 
-    await customDelay(1000);
+    await customDelay(2000);
 
     if (currentHeight === previousHeight) {
       break;
@@ -375,6 +530,36 @@ const scrollToEnd = async (page: Page, box: ElementHandle<HTMLDivElement>) => {
 
     previousHeight = currentHeight ?? 0;
   }
+};
+
+/**
+ * Clean background image url
+ * @param url
+ * @returns
+ */
+const cleanBackgroundImageUrl = (url: string) =>
+  url.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+
+/**
+ * Shuffle array
+ * @param array
+ * @returns
+ */
+const shuffleArray = <T>(array: T[]) => {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 };
 
 /**
